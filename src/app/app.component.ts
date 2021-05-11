c3be6ce502f406d4472ae43a34cc2a57b317a173
daffriedman <dafriedman443@gmail.com>
2021-05-11 23:46:21 +0300

    simple picture with captions on cards showing binding and inputs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cardapp';
+  posts = [
+    {
+      title: 'Neat Tree',
+      imageUrl: 'assets/tree.jpeg',
+      userName: 'nature',
+      content: 'I saw this neat tree today'
+    },
+    {
+      title: 'Snowy Mountain',
+      imageUrl: 'assets/mountain.jpeg',
+      userName: 'mountainlover',
+      content: 'Cool mountain'
+    },
+    {
+      title: 'Mountain Biking',
+      imageUrl: 'assets/biking.jpeg',
+      userName: 'biker',
+      content: 'Great ride today'
+    },
+  ]
 }
